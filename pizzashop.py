77460d038540a079c48ce715651ab27a330f9428
Bhokin <bhokin.w@ku.th>
2021-10-11 10:49:47 +0700

    Rename Method

--- pizzashop.py
+++ pizzashop.py
@@ -9,25 +9,25 @@ from pizza import *
 # - if use type annotation ':Pizza' on the parameter, it should
 
 
-def order_pizza(pizza):
+def order_pizza(item: Pizza):
     """Print a description of a pizza, along with its price."""
 
     # create printable description of the pizza such as
     # "small pizza with muschroom" or "small plain pizza"
-    description = pizza.size
-    if pizza.toppings:
-        description += " pizza with " + ", ".join(pizza.toppings)
+    description = item.size
+    if item.toppings:
+        description += " pizza with " + ", ".join(item.toppings)
     else:
         description += " plain cheeze pizza"
     print(f"A {description}")
-    print("Price:", pizza.getPrice())
+    print("Price:", item.get_price())
 
 
 if __name__ == "__main__":
     pizza = Pizza(SMALL)
-    pizza.addTopping("mushroom")
-    pizza.addTopping("tomato")
-    pizza.addTopping("pinapple")
+    pizza.add_topping("mushroom")
+    pizza.add_topping("tomato")
+    pizza.add_topping("pinapple")
     order_pizza(pizza)
 
     # a plain pizza
@@ -36,5 +36,5 @@ if __name__ == "__main__":
 
     # pizza with only one topping
     pizza3 = Pizza(LARGE)
-    pizza3.addTopping("seafood")
+    pizza3.add_topping("seafood")
     order_pizza(pizza3)
