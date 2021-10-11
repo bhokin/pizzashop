ca8d1285ca36409da06351c1880c69953d784a36
Bhokin <bhokin.w@ku.th>
2021-10-11 10:41:43 +0700

    Replace String Literals with Named Constants

--- pizzashop.py
+++ pizzashop.py
@@ -1,4 +1,4 @@
-from pizza import Pizza
+from pizza import *
 
 # This function shows a limitation on tool-assisted
 # refactoring in a dynamic language like Python.
@@ -8,6 +8,7 @@ from pizza import Pizza
 # - if no type annotation on the pizza parameter, maybe not
 # - if use type annotation ':Pizza' on the parameter, it should
 
+
 def order_pizza(pizza):
     """Print a description of a pizza, along with its price."""
 
@@ -15,7 +16,7 @@ def order_pizza(pizza):
     # "small pizza with muschroom" or "small plain pizza"
     description = pizza.size
     if pizza.toppings:
-        description += " pizza with "+ ", ".join(pizza.toppings)
+        description += " pizza with " + ", ".join(pizza.toppings)
     else:
         description += " plain cheeze pizza"
     print(f"A {description}")
@@ -23,17 +24,17 @@ def order_pizza(pizza):
 
 
 if __name__ == "__main__":
-    pizza = Pizza('small')
+    pizza = Pizza(SMALL)
     pizza.addTopping("mushroom")
     pizza.addTopping("tomato")
     pizza.addTopping("pinapple")
     order_pizza(pizza)
 
     # a plain pizza
-    pizza2 = Pizza("medium")
+    pizza2 = Pizza(MEDIUM)
     order_pizza(pizza2)
 
     # pizza with only one topping
-    pizza3 = Pizza("large")
+    pizza3 = Pizza(LARGE)
     pizza3.addTopping("seafood")
     order_pizza(pizza3)
